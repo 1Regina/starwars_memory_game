91448a12c1a7407d96b01425e250bccb033bf11a
1Regina <1reginacheong@gmail.com>
2022-07-05 23:11:48 +0800

    fix: change port to avoid clash with ecomm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const dotenv = require("dotenv");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
